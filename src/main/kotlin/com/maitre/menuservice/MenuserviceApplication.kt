8a208ae1d8a91f9b850110a6b597dcedf00d1ae7
padovese <bpadovese@uolinc.com>
2021-01-30 09:01:50 -0300

    chore: initial commit

---
+++ src/main/kotlin/com/maitre/menuservice/MenuserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.maitre.menuservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MenuserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MenuserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/maitre/menuservice/MenuserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.maitre.menuservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MenuserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
