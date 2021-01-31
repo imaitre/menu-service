58b5ca3e4b1d9d8247df93d4f98436b24e5d6532
padovese <bpadovese@uolinc.com>
2021-01-31 08:49:33 -0300

    feat: creating domain entities

--- src/main/kotlin/com/maitre/menuservice/adapter/in/dto/GroupRequestDTO.kt
+++ src/main/kotlin/com/maitre/menuservice/adapter/in/dto/GroupRequestDTO.kt
@@ -2,6 +2,7 @@ package com.maitre.menuservice.adapter.`in`.dto
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import com.maitre.menuservice.domain.entity.GroupType
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class GroupRequestDTO(
@@ -11,9 +12,3 @@ data class GroupRequestDTO(
     val available: Boolean,
     val products: List<ProductRequestDTO>?
     )
-
-enum class GroupType {
-    ORDINARY_FOOD, DESSERT, CANDY, FRUIT, PIZZA, HAMBURGER, VEGETARIAN, VEGAN, ICE_CREAM, PASTA,
-    ORDINARY_DRINK, SOFT_DRINK, ALCOHOLIC_DRINK, JUICE, MILKSHAKE,
-    OTHER
-}
---
+++ src/main/kotlin/com/maitre/menuservice/domain/entity/Menu.kt
@@ -0,0 +1,12 @@
+package com.maitre.menuservice.domain.entity
+
+import java.util.UUID
+
+data class Menu(
+    val id: String = "MENU_" + UUID.randomUUID(),
+    val customerId: String,
+    val name: String,
+    val description: String?,
+    val available: Boolean,
+    val groups: List<Group>?
+)
