561120e2eca5934fe634eab63f9e219d1462154c
padovese <bpadovese@uolinc.com>
2021-01-31 11:31:37 -0300

    feat: menu entity and responde dto

---
+++ src/main/kotlin/com/maitre/menuservice/adapter/in/dto/MenuResponseDTO.kt
@@ -0,0 +1,14 @@
+package com.maitre.menuservice.adapter.`in`.dto
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
+data class MenuResponseDTO(
+    val id: String,
+    val customerId: String,
+    val name: String,
+    val description: String?,
+    val available: Boolean,
+    val groups: List<GroupRequestDTO>?
+)
