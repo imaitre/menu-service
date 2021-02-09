1612a9ff560a672ac4f37de120f5db2f766ba8e8
padovese <bpadovese@uolinc.com>
2021-02-08 22:11:13 -0300

    test: mocking mongodb on integration tests

--- src/test/kotlin/com/maitre/menuservice/integration/menu/create/CreateMenuIntegrationTest.kt
+++ src/test/kotlin/com/maitre/menuservice/integration/menu/create/CreateMenuIntegrationTest.kt
@@ -6,6 +6,7 @@ import com.maitre.menuservice.adapter.menu.out.persistence.entity.MenuEntity
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -19,13 +20,13 @@ class CreateMenuIntegrationTest {
     @Autowired
     lateinit var webTestClient: WebTestClient
 
-    @Autowired
+    @Mock
     lateinit var menuRepository: MenuRepository
 
     @Test
     fun `Should create a menu`() {
 
-        whenever(menuRepository.save(any<MenuEntity>())).thenReturn(MenuEntity("ID", "custid", "name", "desc", false).toMono())
+        whenever(menuRepository.save(any<MenuEntity>())).thenReturn(menuEntity.toMono())
 
         val menuRequestDTO = MenuRequestDTO(
             "CUST_0c6e1cb0-df2e-414a-98fb-73b2b8ce6b62",
@@ -49,4 +50,12 @@ class CreateMenuIntegrationTest {
             .jsonPath("$.available").isEqualTo(true)
 
     }
+
+    private val menuEntity = MenuEntity(
+        "MENU_af60830b-d190-43bf-afcb-f5cc2656ea25",
+        "CUST_0c6e1cb0-df2e-414a-98fb-73b2b8ce6b62",
+        "Monday menu",
+        "some description.",
+        true
+    )
 }
--- src/test/kotlin/com/maitre/menuservice/integration/menu/get/GetMenuIntegrationTest.kt
+++ src/test/kotlin/com/maitre/menuservice/integration/menu/get/GetMenuIntegrationTest.kt
@@ -1,9 +1,15 @@
 package com.maitre.menuservice.integration.menu.get
 
+import com.maitre.menuservice.adapter.menu.out.MenuRepository
+import com.maitre.menuservice.adapter.menu.out.persistence.entity.MenuEntity
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.kotlin.core.publisher.toMono
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class GetMenuIntegrationTest {
@@ -11,8 +17,14 @@ class GetMenuIntegrationTest {
     @Autowired
     lateinit var webTestClient: WebTestClient
 
+    @Mock
+    private lateinit var menuRepository: MenuRepository
+
     @Test
     fun `Should get a menu`() {
+
+        whenever(menuRepository.findById(any<String>())).thenReturn(menuEntity.toMono())
+
         webTestClient
             .get()
             .uri("/menu/MENU_af60830b-d190-43bf-afcb-f5cc2656ea25")
@@ -21,8 +33,8 @@ class GetMenuIntegrationTest {
             .expectBody()
             .jsonPath("$.id").isEqualTo("MENU_af60830b-d190-43bf-afcb-f5cc2656ea25")
             .jsonPath("$.customer_id").isEqualTo("CUST_0c6e1cb0-df2e-414a-98fb-73b2b8ce6b62")
-            .jsonPath("$.name").isEqualTo("Menu de segunda")
-            .jsonPath("$.description").isEqualTo("abcd e rf g h hjj dddd")
+            .jsonPath("$.name").isEqualTo("Monday menu")
+            .jsonPath("$.description").isEqualTo("some description.")
             .jsonPath("$.available").isEqualTo(true)
     }
 
@@ -36,7 +48,17 @@ class GetMenuIntegrationTest {
             .expectBody()
             .jsonPath("$.error_messages").isArray
             .jsonPath("$.error_messages.length()").isEqualTo(1)
-            .jsonPath("$.error_messages[*].description").isEqualTo("No menu found. id=MENU_af60830b-d190-43bf-afcb-f5cc2656ea26")
+            .jsonPath("$.error_messages[*].description")
+            .isEqualTo("No menu found. id=MENU_af60830b-d190-43bf-afcb-f5cc2656ea26")
 
     }
+
+    private val menuEntity = MenuEntity(
+        "MENU_af60830b-d190-43bf-afcb-f5cc2656ea25",
+        "CUST_0c6e1cb0-df2e-414a-98fb-73b2b8ce6b62",
+        "Monday menu",
+        "some description.",
+        true
+    )
+
 }
