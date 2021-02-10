872d5f65feb73e8043d36abfd90ad381d02d0d67
Diego Lucas <diego.lucasilva@gmail.com>
2021-02-10 14:56:46 -0300

    refactoring tests

--- src/test/kotlin/com/maitre/menuservice/adapter/group/in/get/GetGroupHandlerTest.kt
+++ src/test/kotlin/com/maitre/menuservice/adapter/group/in/get/GetGroupHandlerTest.kt
@@ -4,14 +4,16 @@ import com.maitre.menuservice.adapter.menu.`in`.create.GetGroupHandlerImpl
 import com.maitre.menuservice.domain.group.entity.Group
 import com.maitre.menuservice.domain.group.entity.GroupType
 import com.maitre.menuservice.domain.group.usecases.GetGroupsByMenuIdUseCase
-import com.nhaarman.mockitokotlin2.*
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.ServerRequest
 import reactor.core.publisher.Flux
-import java.util.*
+import java.util.Optional
 
 internal class GetGroupHandlerTest{
 
@@ -51,8 +53,7 @@ internal class GetGroupHandlerTest{
         whenever(serverRequest.queryParam("menu_id")).thenReturn(Optional.of("MENU_af60830b-d190-43bf-afcb-f5cc2656ea25"))
         whenever(getGroupsByMenuIdUseCase.execute(any<String>())).thenReturn(Flux.just(group))
     }
-
-
+    
     private val group = Group(
             "GRUO_bc4743a8-1130-4127-a057-0aacc950a1e3",
             "MENU_af60830b-d190-43bf-afcb-f5cc2656ea25",
