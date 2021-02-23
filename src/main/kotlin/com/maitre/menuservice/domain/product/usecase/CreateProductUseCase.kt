1c6fb5a2c4b40139d15c758e43f8aa7506af135a
padovese <bpadovese@uolinc.com>
2021-02-23 12:21:19 -0300

    feat: verify if group exists when save a product

--- src/main/kotlin/com/maitre/menuservice/domain/product/usecase/CreateProductUseCase.kt
+++ src/main/kotlin/com/maitre/menuservice/domain/product/usecase/CreateProductUseCase.kt
@@ -1,7 +1,9 @@
 package com.maitre.menuservice.domain.product.usecase
 
+import com.maitre.menuservice.domain.group.port.out.persistence.GetGroupByIdPort
 import com.maitre.menuservice.domain.product.entity.Product
 import com.maitre.menuservice.domain.product.port.out.persistence.SaveProductPort
+import com.maitre.menuservice.exception.GroupNotFoundException
 import com.maitre.menuservice.utils.toJson
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
@@ -10,12 +12,15 @@ import reactor.core.publisher.Mono
 @Service
 class CreateProductUseCase(
     private val logger: Logger,
-    private val saveProductPort: SaveProductPort
+    private val saveProductPort: SaveProductPort,
+    private val getGroupByIdPort: GetGroupByIdPort
 ) {
 
     fun execute(product: Product): Mono<Product> {
         logger.info("Create product use case initiated. menu=${product.toJson()}")
-        return saveProductPort.save(product)
+        return getGroupByIdPort.getById(product.groupId)
+            .switchIfEmpty(Mono.error(GroupNotFoundException(product.groupId)))
+            .then(saveProductPort.save(product))
             .doOnNext { logger.info("Create product use case done.") }
     }
 }
