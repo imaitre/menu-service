a67612a82d76a54febe695115e2c4e2d963a2d86
Diego Lucas <diego.lucasilva@gmail.com>
2021-03-07 18:27:34 -0300

    domain get optionals bt id

--- src/main/kotlin/com/maitre/menuservice/adapter/optional/OptionalAdapter.kt
+++ src/main/kotlin/com/maitre/menuservice/adapter/optional/OptionalAdapter.kt
@@ -2,18 +2,25 @@ package com.maitre.menuservice.adapter.optional
 
 import com.maitre.menuservice.adapter.optional.out.persistence.entity.OptionalRepository
 import com.maitre.menuservice.domain.optional.entity.Optional
+import com.maitre.menuservice.domain.optional.port.out.persistence.GetOptionalsByMenuIdPort
 import com.maitre.menuservice.domain.optional.port.out.persistence.SaveOptionalPort
 import com.maitre.menuservice.utils.toDomain
 import com.maitre.menuservice.utils.toEntity
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 
 @Component
-class OptionalAdapter(private val optionalRepository: OptionalRepository): SaveOptionalPort {
+class OptionalAdapter(private val optionalRepository: OptionalRepository): SaveOptionalPort, GetOptionalsByMenuIdPort {
 
   override fun save(optional: Optional): Mono<Optional> {
     return optionalRepository.save(optional.toEntity())
+      .map { it.toDomain() }
+  }
+
+  override fun getByMenuId(menuId: String): Flux<Optional> {
+    return optionalRepository.findAllByMenuId(menuId)
       .map { it.toDomain() }
   }
 }
--- src/main/kotlin/com/maitre/menuservice/adapter/optional/out/persistence/entity/OptionalRepository.kt
+++ src/main/kotlin/com/maitre/menuservice/adapter/optional/out/persistence/entity/OptionalRepository.kt
@@ -2,7 +2,11 @@ package com.maitre.menuservice.adapter.optional.out.persistence.entity
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
 @Repository
 interface OptionalRepository: ReactiveMongoRepository<OptionalEntity, String> {
+
+  fun findAllByMenuId(id: String): Flux<OptionalEntity>
+
 }
